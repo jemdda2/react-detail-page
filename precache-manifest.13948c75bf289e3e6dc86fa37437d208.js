ee4121969944c3d2424e7880003a1d7bd62d665c
Jisoon <siriusda2@naver.com>
2020-07-12 13:59:45 +0900

    Updates

---
+++ precache-manifest.13948c75bf289e3e6dc86fa37437d208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b9f2dcb7873531ea400eeea100ac38",
+    "url": "/react-detail-page/index.html"
+  },
+  {
+    "revision": "393084105604769946fe",
+    "url": "/react-detail-page/static/css/main.49662a44.chunk.css"
+  },
+  {
+    "revision": "2c98802f28998f8551a5",
+    "url": "/react-detail-page/static/js/2.f2bf4f63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-detail-page/static/js/2.f2bf4f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393084105604769946fe",
+    "url": "/react-detail-page/static/js/main.3e966fee.chunk.js"
+  },
+  {
+    "revision": "2e547c8cc9b901fddadf",
+    "url": "/react-detail-page/static/js/runtime-main.b015b925.js"
+  }
+]);
